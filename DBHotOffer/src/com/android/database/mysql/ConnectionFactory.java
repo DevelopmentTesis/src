a10f446a758763cce3db1455e4de24cdbbdabde6
cesar <cesar.araya.acosta@gmail.com>
2013-07-16 23:01:41 -0400

    .

--- DBHotOffer/src/com/android/database/mysql/ConnectionFactory.java
+++ DBHotOffer/src/com/android/database/mysql/ConnectionFactory.java
@@ -9,13 +9,6 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.apache.log4j.Logger;
 
-/**
- * MyBatis Connection Factory, which reads the configuration data from a XML
- * file.
- * 
- * @author Loiane Groner http://loianegroner.com (English) http://loiane.com
- *         (Portuguese)
- */
 public class ConnectionFactory {
 
 	private static SqlSessionFactory sqlSessionFactory;
